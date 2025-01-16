e35c50615aa7107b2439ed582ef7c1208311d903
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2025-01-16 06:55:37 +0100

    Remove TripContext as it's no longer needed

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { TripProvider } from "@/contexts/trip-context";
 import "./index.css";
 import { App } from "./app.tsx";
 import "react-datepicker/dist/react-datepicker.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<TripProvider>
-			<App />
-		</TripProvider>
+		<App />
 	</StrictMode>,
 );
