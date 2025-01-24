533b2d0f58454ce42bc9b20bcea3746fa3d1d78c
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2025-01-24 14:05:40 +0100

    Add handleDownload utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -49,3 +49,12 @@ export function formatDistance(distance: number, unit: DistanceUnit) {
 			return `${distance.toLocaleString()} mi`;
 	}
 }
+
+export const handleDownload = (fileName: string, blob: Blob) => {
+	const elem = window.document.createElement("a");
+	elem.href = window.URL.createObjectURL(blob);
+	elem.download = fileName;
+	document.body.appendChild(elem);
+	elem.click();
+	document.body.removeChild(elem);
+};
