bfc827704e38b0abd61b20bed1162af801817ec3
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2025-02-02 15:48:36 +0100

    Add and configure TanStack Query

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,13 +1,17 @@
 import { DefaultCatchBoundary } from "@/components/default-catch-boundary";
 import { NotFound } from "@/components/not-found";
 import appCss from "@/styles/app.css?url";
-import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { type QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "react-datepicker/dist/react-datepicker.css";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Meta, Scripts } from "@tanstack/start";
 import { PropsWithChildren } from "react";
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+	queryClient: QueryClient;
+}>()({
 	component: RootComponent,
 	errorComponent: (props) => (
 		<RootDocument>
@@ -36,7 +40,8 @@ function RootComponent() {
 	return (
 		<RootDocument>
 			<Outlet />
-			<TanStackRouterDevtools />
+			<TanStackRouterDevtools position="bottom-right" />
+			<ReactQueryDevtools buttonPosition="bottom-left" />
 		</RootDocument>
 	);
 }
