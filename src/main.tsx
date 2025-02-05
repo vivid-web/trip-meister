6742f348e506754beb058462de1bdb2c755b65e5
Peter van Meijgaard <peter@vivid-web.com>
2025-02-05 09:07:48 +0100

    Add and configure TanStack Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,33 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.css";
-import { App } from "./app";
-
+import { routeTree } from "./routeTree.gen";
+import "./styles.css";
 import "react-datepicker/dist/react-datepicker.css";
 
+const router = createRouter({
+	defaultPreload: "intent",
+	routeTree,
+	scrollRestoration: true,
+});
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+const rootElement = document.getElementById("root")!;
+
+if (!rootElement.innerHTML) {
+	const root = createRoot(rootElement);
+
+	root.render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	);
+}
+
+declare module "@tanstack/react-router" {
+	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+	interface Register {
+		router: typeof router;
+	}
+}
