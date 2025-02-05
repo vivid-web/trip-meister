e65111cdeab328ff19169dee5792bed82bf971e6
Peter van Meijgaard <peter@vivid-web.com>
2025-02-05 09:07:48 +0100

    Add and configure TanStack Router

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -5,7 +5,7 @@ function Card({ className, ...props }: ComponentProps<"div">) {
 	return (
 		<div
 			className={cn(
-				"rounded-lg border bg-card text-card-foreground shadow-sm",
+				"bg-card text-card-foreground rounded-lg border shadow-sm",
 				className,
 			)}
 			{...props}
@@ -26,7 +26,7 @@ function CardTitle({ className, ...props }: ComponentProps<"div">) {
 	return (
 		<div
 			className={cn(
-				"text-2xl leading-none font-semibold tracking-tight",
+				"text-2xl font-semibold leading-none tracking-tight",
 				className,
 			)}
 			{...props}
@@ -37,7 +37,7 @@ function CardTitle({ className, ...props }: ComponentProps<"div">) {
 function CardDescription({ className, ...props }: ComponentProps<"div">) {
 	return (
 		<div
-			className={cn("text-sm text-muted-foreground", className)}
+			className={cn("text-muted-foreground text-sm", className)}
 			{...props}
 		/>
 	);
