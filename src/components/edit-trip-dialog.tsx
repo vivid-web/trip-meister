c289df37290a50fd61315cdaa45854e63e135b1f
Peter van Meijgaard <peter@vivid-web.com>
2025-02-05 08:05:45 +0100

    Move schemas to own file

--- src/components/edit-trip-dialog.tsx
+++ src/components/edit-trip-dialog.tsx
@@ -23,31 +23,13 @@ import { db, Trip } from "@/db";
 import { toast } from "sonner";
 import { useAtom } from "jotai/index";
 import { distanceUnitsAtom } from "@/atoms";
-
-const NameSchema = z
-	.string({ message: "The trip name is required" })
-	.min(5, "The trip name must be longer than 5 character")
-	.max(255, "The trip name must be shorter than 255 characters");
-
-const StartDateSchema = z.coerce.date({
-	message: "The start date must be a valid date",
-	required_error: "The start date is required",
-	invalid_type_error: "The start date must be a valid date",
-});
-
-const EndDateSchema = z.coerce.date({
-	message: "The end date must be a valid date",
-	required_error: "The end date is required",
-	invalid_type_error: "The end date must be a valid date",
-});
-
-const StartMileageSchema = z.coerce
-	.number({ message: "The start mileage is required" })
-	.min(0, "The start mileage must be greater than 0");
-
-const EndMileageSchema = z.coerce
-	.number({ message: "The end mileage is required" })
-	.min(0, "The end mileage must be greater than 0");
+import {
+	EndDateSchema,
+	EndMileageSchema,
+	NameSchema,
+	StartDateSchema,
+	StartMileageSchema,
+} from "@/lib/schemas";
 
 export function EditTripDialog({
 	children,
