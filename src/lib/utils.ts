9ba83bf9773ab2382c1d6d8fbd9d67280eb7dc62
Peter van Meijgaard <peter@vivid-web.com>
2025-02-05 08:05:27 +0100

    Add form UI component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { Context, useContext } from "react";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export const useSafeContext = <T>(context: Context<T | undefined>) => {
+	const value = useContext(context);
+
+	if (!value) {
+		throw new Error("Component must be wrapped with a provider");
+	}
+
+	return value;
+};
