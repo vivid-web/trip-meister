44550d57915c3ff44417c80c039691671d82ee46
Peter van Meijgaard <peter@vivid-web.com>
2025-02-05 08:05:27 +0100

    Add label UI component

---
+++ src/components/ui/label.tsx
@@ -0,0 +1,23 @@
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+import { ComponentProps } from "react";
+
+const labelVariants = cva(
+	"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+);
+
+type LabelProps = ComponentProps<typeof LabelPrimitive.Root> &
+	VariantProps<typeof labelVariants>;
+
+function Label({ className, ...props }: LabelProps) {
+	return (
+		<LabelPrimitive.Root
+			className={cn(labelVariants(), className)}
+			{...props}
+		/>
+	);
+}
+
+export { Label };
