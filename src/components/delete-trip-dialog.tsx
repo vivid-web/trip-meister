d2b4abb63f3b8a84e599ee97ab006e296dcc6cdf
Peter van Meijgaard <peter@vivid-web.com>
2025-02-07 21:10:51 +0100

    Remove file extensions from imports

--- src/components/delete-trip-dialog.tsx
+++ src/components/delete-trip-dialog.tsx
@@ -8,7 +8,7 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from "@/components/ui/dialog";
-import { db, Trip } from "@/db.ts";
+import { db, Trip } from "@/db";
 import { PropsWithChildren } from "react";
 import { toast } from "sonner";
 
