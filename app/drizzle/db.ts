01c5ab511df3115c9cd861f18f6388a1205405b4
Peter van Meijgaard <peter@vivid-web.com>
2025-02-07 22:15:23 +0100

    Add auth schemas with migrations

--- app/drizzle/db.ts
+++ app/drizzle/db.ts
@@ -1,10 +1,13 @@
 import { env } from "@/lib/server/env";
 import { drizzle } from "drizzle-orm/libsql";
 
+import * as schema from "./schema";
+
 export const db = drizzle({
 	casing: "snake_case",
 	connection: {
 		authToken: env.TURSO_AUTH_TOKEN,
 		url: env.TURSO_DATABASE_URL,
 	},
+	schema,
 });
