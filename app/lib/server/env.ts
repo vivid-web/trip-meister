68b30ccc8b924788cf43b277e8127fa57e875c01
Peter van Meijgaard <peter@vivid-web.com>
2025-02-07 22:06:43 +0100

    Add and configure better auth

--- app/lib/server/env.ts
+++ app/lib/server/env.ts
@@ -6,10 +6,16 @@ export const env = createEnv({
 
 	runtimeEnv: process.env,
 
+	/* eslint-disable perfectionist/sort-objects */
 	server: {
+		// Better Auth
+		BETTER_AUTH_URL: z.string().url(),
+		BETTER_AUTH_SECRET: z.string().min(1),
+
 		// Database
 		DATABASE_PATH: z.string().min(1),
 	},
+	/* eslint-enable perfectionist/sort-objects */
 
 	skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });
