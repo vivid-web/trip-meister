f6fc5f2cfd0d0b04dacb2d5c51e2d63ae73093c3
Peter van Meijgaard <peter@vivid-web.com>
2025-02-08 17:02:33 +0100

    Add Google auth provider

--- app/lib/server/env.ts
+++ app/lib/server/env.ts
@@ -16,6 +16,10 @@ export const env = createEnv({
 		GITHUB_CLIENT_ID: z.string().min(1),
 		GITHUB_CLIENT_SECRET: z.string().min(1),
 
+		// Google
+		GOOGLE_CLIENT_ID: z.string().min(1),
+		GOOGLE_CLIENT_SECRET: z.string().min(1),
+
 		// Turso
 		TURSO_DATABASE_URL: z.string().url(),
 		TURSO_AUTH_TOKEN: z.string().min(1).optional(),
