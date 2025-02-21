081c17d69bd916a999c72287a3da69f5c971f50b
Peter van Meijgaard <peter@vivid-web.com>
2025-02-21 16:08:04 +0100

    Ignore ESLint rule

--- app/entry.ssr.tsx
+++ app/entry.ssr.tsx
@@ -11,4 +11,5 @@ import { createRouter } from "./router";
 export default createStartHandler({
 	createRouter,
 	getRouterManifest,
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 })(defaultStreamHandler);
